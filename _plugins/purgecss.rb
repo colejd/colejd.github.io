e9c0f32b80c26b5eeb97dc80bbcbca15bd427c45
Jonathan Cole <joncole04412@gmail.com>
2021-01-13 19:36:20 -0500

    Use local purgecss instead of global, restore from cache in CI

--- _plugins/purgecss.rb
+++ _plugins/purgecss.rb
@@ -1,4 +1,5 @@
 # Loosely adapted from https://www.seancdavis.com/blog/use-purge-css-to-optimize-jekyll/
 Jekyll::Hooks.register(:site, :post_write) do |_site|
-    system("purgecss --css #{Dir.glob('_site/assets/css/*.css').first} --content _site/**/*.html --output #{Dir.glob('_site/assets/css/*.css').first}")
+    # https://stackoverflow.com/a/15157360/7138792
+    system("PATH=$(npm bin):$PATH purgecss --css #{Dir.glob('_site/assets/css/*.css').first} --content _site/**/*.html --output #{Dir.glob('_site/assets/css/*.css').first}")
 end
