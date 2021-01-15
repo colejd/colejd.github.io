9bf9702ab968a8557de23cdc3b3f5c1ad8fd081e
Jonathan Cole <joncole04412@gmail.com>
2021-01-15 15:13:59 -0500

    Revert "Try out Sass (tailwind css tokens don't work in sass for some reason)"
    This reverts commit 96f1960d023577854743f290a7039a26f42a96a8.

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-    options: {
-      parser: require('postcss-scss')
-    },
     plugins: [
-    //   require('postcss-import'),
+      require('postcss-import'),
       require('tailwindcss'),
-    //   require('postcss-preset-env')({ stage: 2 }), // Does the work of autoprefixer, postcss-custom-properties, and postcss-nesting
+      require('postcss-preset-env')({ stage: 2 }), // Does the work of autoprefixer, postcss-custom-properties, and postcss-nesting
       ...(process.env.JEKYLL_ENV == "production"
         ? [require('cssnano')({ preset: 'default' })]
         : [])
