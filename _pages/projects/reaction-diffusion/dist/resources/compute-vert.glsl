35a33ae49c4f3bedab49f84dc000e7707ec9fa4c
Jonathan Cole <joncole04412@gmail.com>
2021-02-05 19:58:32 -0500

    Update to reaction-diffusion-threejs 1.1

--- _pages/projects/reaction-diffusion/dist/resources/compute-vert.glsl
+++ _pages/projects/reaction-diffusion/dist/resources/compute-vert.glsl
@@ -1,6 +1,11 @@
 varying vec2 v_uv;
-varying vec2 texelSize;
 
+varying vec2 left_coord;
+varying vec2 right_coord;
+varying vec2 top_coord;
+varying vec2 bottom_coord;
+
+uniform vec2 texelSize;
 uniform vec2 resolution;
 
 // Provided by Three.js:
@@ -9,11 +14,15 @@ uniform vec2 resolution;
 // uniform mat4 modelViewMatrix;
 
 void main() {
-    texelSize = 1.0 / resolution.xy;
-    texelSize *= 1.0; //Default 1.0. Change the multiplier for fun times
-
     v_uv = uv;
 
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    // Compute coordinates of neighbor texels here to save on perf
+    // (If many pixels correspond to one texel we save on texture reads)
+    // Orthogonal texels
+    bottom_coord = uv + vec2( 0.0, texelSize.y );
+    top_coord = uv + vec2( 0.0, -texelSize.y );
+    right_coord = uv + vec2( texelSize.x, 0.0 );
+    left_coord = uv + vec2( -texelSize.x, 0.0 );
 
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
