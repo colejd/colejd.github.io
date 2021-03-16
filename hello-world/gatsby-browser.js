22bb751a79bdd9938696858d6915759d490554b3
Jonathan Cole <joncole04412@gmail.com>
2021-03-16 17:20:30 -0400

    Style code blocks, resolve warnings

--- hello-world/gatsby-browser.js
+++ hello-world/gatsby-browser.js
@@ -4,4 +4,7 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+import "./src/styles/global.css"
+
+require("prismjs/themes/prism-solarizedlight.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
