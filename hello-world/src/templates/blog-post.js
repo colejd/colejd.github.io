c3c70399cb4524a5bef818d5de5be88364cf5ae0
Jonathan Cole <joncole04412@gmail.com>
2021-03-19 18:42:31 -0400

    Improve code block styling and pick theme

--- hello-world/src/templates/blog-post.js
+++ hello-world/src/templates/blog-post.js
@@ -5,7 +5,7 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-require("prismjs/themes/prism-solarizedlight.css")
+require("prismjs/themes/prism-okaidia.css")
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
 require("prismjs/plugins/command-line/prism-command-line.css")
 
