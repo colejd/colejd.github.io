1e389037b0db17907a8703885c7daffbbb5b6883
Jonathan Cole <joncole04412@gmail.com>
2021-03-22 17:36:03 -0400

    Run formatter on previews

--- hello-world/src/components/blog-post-preview.js
+++ hello-world/src/components/blog-post-preview.js
@@ -5,22 +5,25 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 const BlogPostPreview = ({ post }) => (
   <section className="mb-12">
     <header className="flex flex-col mb-2">
-        { post.frontmatter.coverPhoto != null ?
-            <Link to={post.fields.slug} className="text-2xl">
-                <GatsbyImage image={getImage(post.frontmatter.coverPhoto)} alt={post.frontmatter.coverPhotoAlt} className="mb-2" />
-            </Link>
-            : ""
-        }
+      {post.frontmatter.coverPhoto != null ? (
         <Link to={post.fields.slug} className="text-2xl">
-            {post.frontmatter.title}
+          <GatsbyImage
+            image={getImage(post.frontmatter.coverPhoto)}
+            alt={post.frontmatter.coverPhotoAlt}
+            className="mb-2"
+          />
         </Link>
-        <time datetime={post.frontmatter.isoDate} className="text-sm">
-            {post.frontmatter.readableDate}
-        </time>
+      ) : (
+        ""
+      )}
+      <Link to={post.fields.slug} className="text-2xl">
+        {post.frontmatter.title}
+      </Link>
+      <time datetime={post.frontmatter.isoDate} className="text-sm">
+        {post.frontmatter.readableDate}
+      </time>
     </header>
-    <div className="mt-1">
-        {post.excerpt}
-    </div>
+    <div className="mt-1">{post.excerpt}</div>
   </section>
 )
 export default BlogPostPreview
--- hello-world/src/components/project-preview.js
+++ hello-world/src/components/project-preview.js
@@ -5,20 +5,23 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const ProjectPreview = ({ project }) => (
   <section className="mb-8">
-  <header className="flex flex-col mb-2">
-    { project.frontmatter.coverPhoto != null ?
+    <header className="flex flex-col mb-2">
+      {project.frontmatter.coverPhoto != null ? (
         <Link to={project.fields.slug}>
-          <GatsbyImage image={getImage(project.frontmatter.coverPhoto)} alt={project.frontmatter.coverPhotoAlt} className="mb-2" />
+          <GatsbyImage
+            image={getImage(project.frontmatter.coverPhoto)}
+            alt={project.frontmatter.coverPhotoAlt}
+            className="mb-2"
+          />
         </Link>
-        : ""
-    }
-    <Link to={project.fields.slug} className="text-2xl">
+      ) : (
+        ""
+      )}
+      <Link to={project.fields.slug} className="text-2xl">
         {project.frontmatter.title}
-    </Link>
-    <div className="text-sm">
-      {project.frontmatter.subtitle}
-    </div>
-  </header>
-</section>
+      </Link>
+      <div className="text-sm">{project.frontmatter.subtitle}</div>
+    </header>
+  </section>
 )
 export default ProjectPreview
