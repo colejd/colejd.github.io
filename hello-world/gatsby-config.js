b709a7f8715a6be431df345cd511259057663a8d
Jonathan Cole <joncole04412@gmail.com>
2021-03-22 14:06:39 -0400

    Autolink markdown headers

--- hello-world/gatsby-config.js
+++ hello-world/gatsby-config.js
@@ -77,6 +77,7 @@ module.exports = {
             resolve: 'gatsby-remark-responsive-iframe',
             options: { wrapperStyle: 'margin-bottom: 1.0725rem' }
           },
+          `gatsby-remark-autolink-headers`,
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
