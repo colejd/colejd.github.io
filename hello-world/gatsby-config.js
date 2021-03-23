9c21f6f9c2a9b465faf26b32fc99b2467324a677
Jonathan Cole <joncole04412@gmail.com>
2021-03-23 16:25:55 -0400

    Add RSS feed generation

--- hello-world/gatsby-config.js
+++ hello-world/gatsby-config.js
@@ -172,5 +172,61 @@ module.exports = {
         exclude: [],
       },
     },
+
+    // RSS feed generation
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                  filter: { fileAbsolutePath: {regex: "/(blog)/"}, frontmatter: {draft: { ne: true }} },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Jon's Blog",
+          },
+        ],
+      },
+    },
   ],
 }
