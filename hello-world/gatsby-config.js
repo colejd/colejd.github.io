e771d4e9e59f5cbb7c6d334cbde1335840b7dbb1
Jonathan Cole <joncole04412@gmail.com>
2021-03-23 15:51:11 -0400

    Add rel=canonical to all pages

--- hello-world/gatsby-config.js
+++ hello-world/gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://jons.website`,
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: 'gatsby-source-filesystem',
