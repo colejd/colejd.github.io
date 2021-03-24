ea4e816011baa571b6a739fb9f9f64f4ac9b2fae
Toni Kaplan <tonarie.k@gmail.com>
2021-03-23 20:01:28 -0400

    add scripts frontmatter

--- src/templates/project-page.js
+++ src/templates/project-page.js
@@ -5,37 +5,35 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import "./project-page.css"
+import useScripts from "../utils/hooks/use-scripts"
 
-class ProjectPageTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
+const ProjectPageTemplate = props => {
+  const post = props.data.markdownRemark
+  const siteTitle = props.data.site.siteMetadata.title
+  useScripts(post.frontmatter.scripts)
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <article>
+        <header>
+          <h1 className="text-3xl columnbreak:text-5xl mb-0">
+            {post.frontmatter.title}
+          </h1>
+          <p className="leading-loose mb-8 ">
+            <em>{post.frontmatter.subtitle}</em>
+          </p>
+        </header>
+        <section
+          className="markdown"
+          dangerouslySetInnerHTML={{ __html: post.html }}
         />
-        <article>
-          <header>
-            <h1 className="text-3xl columnbreak:text-5xl mb-0">
-              {post.frontmatter.title}
-            </h1>
-            <p className="leading-loose mb-8 ">
-              <em>
-                {post.frontmatter.subtitle}
-              </em>
-            </p>
-          </header>
-          <section
-            className="markdown"
-            dangerouslySetInnerHTML={{ __html: post.html }}
-          />
-        </article>
-      </Layout>
-    )
-  }
+      </article>
+    </Layout>
+  )
 }
 
 export default ProjectPageTemplate
@@ -53,6 +51,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
+        scripts
         subtitle
         date(formatString: "MMMM DD, YYYY")
         description
