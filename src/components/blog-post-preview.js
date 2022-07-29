f04dce5d9d8f248b2336e128f8d3837bc3678d32
Jonathan Cole <joncole04412@gmail.com>
2022-07-29 14:29:01 -0400

    Improve blog post preview styling

--- src/components/blog-post-preview.js
+++ src/components/blog-post-preview.js
@@ -3,27 +3,26 @@ import React from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const BlogPostPreview = ({ post }) => (
-  <section className="mb-16">
-    <header className="flex flex-col mb-2">
-      <a href={post.fields.slug} className="text-2xl">
-        {post.frontmatter.title}
-      </a>
-      <time dateTime={post.frontmatter.isoDate} className="text-sm">
-        {post.frontmatter.readableDate}
-      </time>
-      {post.frontmatter.coverPhoto != null ? (
+  <section className="mb-8">
+    <div className="rounded-lg bg-gray-200 dark:bg-gray-800 p-4">
+      <header className="flex flex-col mb-2">
         <a href={post.fields.slug} className="text-2xl">
-          <GatsbyImage
-            image={getImage(post.frontmatter.coverPhoto)}
-            alt={post.frontmatter.coverPhotoAlt ?? ""}
-            className="mb-0 mt-2"
-          />
+          {post.frontmatter.title}
         </a>
-      ) : (
-        ""
-      )}
-    </header>
-    <div className="">{post.excerpt}</div>
+        {post.frontmatter.coverPhoto != null ? (
+          <a href={post.fields.slug} className="text-2xl">
+            <GatsbyImage
+              image={getImage(post.frontmatter.coverPhoto)}
+              alt={post.frontmatter.coverPhotoAlt ?? ""}
+              className="mb-0 mt-2"
+            />
+          </a>
+        ) : (
+          ""
+        )}
+      </header>
+      <div className="">{post.excerpt}</div>
+    </div>
   </section>
 )
 export default BlogPostPreview
