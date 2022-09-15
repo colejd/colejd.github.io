135ed698827e6056413082ffb65ab064c5ad1cdb
Jonathan Cole <jon@jons.website>
2022-09-15 18:36:52 -0400

    Redo front page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,21 +36,12 @@ const IndexPage = () => (
     I help build world-class iOS, augmented reality and Unreal Engine apps as a Lead Developer at <a href="https://www.rightpoint.com/">Rightpoint (formerly Raizlabs)</a>. Prior to that, I was part of the <a href="https://umaine.edu/vemi/">Virtual Environment and Multimodal Interaction Laboratory</a>, where I worked at the forefront of human-computer interaction and psychospatial research with VR and AR. I'm skilled with iOS, AR, VR, and computer vision.
     </p>
 
-    <p style={{"marginBottom": 0}}>
-    I like hard problems, and I've worked on a lot of them. Some of them include:
+    <p>
+    I've worked on a lot of stuff, from robotics, computer vision, Internet of Things devices, interactive museum exhibits, <a href="https://cheese.team">game development</a>, building my own VR headset, and so much more. I built this website, too! A lot my work is open-source on <a href="https://github.com/colejd">my GitHub profile</a> if you'd like to have a look. 
     </p>
-    <ul style={{"marginTop": "0.1em"}}>
-    <li className="emoji-bullet emoji-bullet-pizza"><a href="/apps/liquid-math">A Metal-powered Reaction-Diffusion simulator for iOS and macOS</a></li>
-    <li className="emoji-bullet emoji-bullet-film-reel"><a href="https://github.com/colejd/waldorf">A full-stack video annotation solution for preserving and categorizing historical footage</a></li>
-    <li className="emoji-bullet emoji-bullet-sunglasses"><a href="https://github.com/colejd/kino">Software and hardware that helps researchers use AR to study perception and aging</a></li>
-    <li className="emoji-bullet emoji-bullet-car"><a href="https://cugr.umaine.edu/wp-content/uploads/sites/146/2013/04/2013_CUGR-ShowcaseAbstracts1.pdf">A fully-immersive VR driving simulator for studying compensatory augmented reality</a>, complete with a real car seat</li>
-    <li className="emoji-bullet emoji-bullet-signpost">An iOS app that helps blind and low-vision people find bus stops with Bluetooth ranging</li>
-    <li className="emoji-bullet emoji-bullet-light-bulb">The next generation of smart lighting for iOS</li>
-    <li className="emoji-bullet emoji-bullet-robot">An occupancy grid vectorizer and visualizer for cleaning robots</li>
-    </ul>
 
     <p>
-    That's just a surface look at my work, a lot of which is open-source on <a href="https://github.com/colejd">GitHub</a>. If you're interested, you can see more about me on my <a href="/resume/">resume</a>, or <a href="mailto:jon@jons.website">get in touch via email</a>.
+    If you're interested, you can learn more about me on my <a href="/resume/">resume</a>, and you can <a href="mailto:jon@jons.website">get in touch via email here</a>.
     </p>
   </Layout>
 )
