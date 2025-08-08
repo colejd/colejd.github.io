906640e5cc66eb265df8d77d95c9af14e5839a03
Jonathan Cole <jon@jons.website>
2025-08-08 11:29:04 -0400

    Port tailwind config from old js to new css method

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,9 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', 
+    './src/**/*.{js,jsx,ts,tsx}',
     './content/**/*.md'
   ], // TODOjon: Do I need to point at blog posts?
-  theme: {
-    extend: {
-      // Add new colors
-      colors: {
-        brand: {
-          light: colors.blue[500],
-          DEFAULT: '#1fb6ff',
-          dark: '#009eeb',
-        },
-      },
-
-      // Add custom breakpoints
-      screens: {
-        '65ch': '65ch', // Breakpoint between sm and md. Identifies when the main column has hit its max width.
-        'columnbreak': {'raw': '(min-width: 80ch)'}, // Breakpoint for switching from single- to multi-column
-        'widescreen': {'raw': '(min-aspect-ratio: 16/8)'}, // Breakpoint for widescreen monitor aspect ratio
-        'ultrawide': {'raw': '(min-aspect-ratio: 21/8)'}, // Breakpoint for ultrawide monitor aspect ratio
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 }
